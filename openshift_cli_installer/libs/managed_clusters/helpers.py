9b299e02239a4cbfe8204f275868868edbd84210
oharan2 <oharan@redhat.com>
2023-09-20 21:45:39 +0300

    Update aws args defaults to None, remove raise

--- openshift_cli_installer/libs/managed_clusters/helpers.py
+++ openshift_cli_installer/libs/managed_clusters/helpers.py
@@ -1,6 +1,5 @@
 import json
 from datetime import datetime, timedelta
-import click
 
 from openshift_cli_installer.utils.const import (
     AWS_OSD_STR,
@@ -8,16 +7,15 @@ from openshift_cli_installer.utils.const import (
     HYPERSHIFT_STR,
     ROSA_STR,
     TIMEOUT_60MIN,
-    ERROR_LOG_COLOR,
 )
 from openshift_cli_installer.utils.general import tts
 
 
 def prepare_managed_clusters_data(
     clusters,
-    aws_account_id,
-    aws_secret_access_key,
-    aws_access_key_id,
+    aws_account_id=None,
+    aws_secret_access_key=None,
+    aws_access_key_id=None,
     gcp_service_account_file=None,
 ):
     for _cluster in clusters:
@@ -39,16 +37,10 @@ def prepare_managed_clusters_data(
                 _cluster["machine-cidr"] = _cluster.get("cidr", "10.0.0.0/16")
 
         elif cluster_platform == GCP_OSD_STR:
-            if gcp_service_account_file:
-                gcp_service_account_dict = get_service_account_dict_from_file(
-                    gcp_service_account_file=gcp_service_account_file
-                )
-            else:
-                click.secho(
-                    "'gcp_service_account_file' wasn't provided, but cluster_platform is gcp-osd",
-                    fg=ERROR_LOG_COLOR,
-                )
-                raise click.Abort()
+            gcp_service_account_dict = get_service_account_dict_from_file(
+                gcp_service_account_file=gcp_service_account_file
+            )
+
             _cluster["gcp_service_account"] = gcp_service_account_dict
 
         expiration_time = _cluster.get("expiration-time")
