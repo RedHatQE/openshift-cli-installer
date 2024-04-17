b06c8350ff47391e3c3dfc8622b68c7fddc3d8d9
Meni Yakove <myakove@gmail.com>
2024-04-17 23:24:45 +0300

    merge duplicate code

---
+++ openshift_cli_installer/tests/cluster_version/test_osd_rosa_versions.py
@@ -0,0 +1,102 @@
+import click
+import pytest
+
+from openshift_cli_installer.tests.cluster_version.rosa_osd_base_versions import ROSA_OSD_BASE_VERSIONS
+from openshift_cli_installer.utils.cluster_versions import (
+    get_cluster_version_to_install,
+)
+
+PARAMETRIZE_TESTS = [
+    ([{"version": "4.15", "stream": "stable", "expected": "4.15.8"}]),
+    ([
+        {"version": "4.15", "stream": "stable", "expected": "4.15.8"},
+        {
+            "version": "4.16",
+            "stream": "nightly",
+            "expected": "4.16.0-0.nightly-2024-04-16-195622",
+        },
+    ]),
+    ([
+        {
+            "version": "4.16.0-0.nightly-2024-04-16-195622",
+            "stream": "nightly",
+            "expected": "4.16.0-0.nightly-2024-04-16-195622",
+        }
+    ]),
+    ([{"version": "4.15.8", "stream": "stable", "expected": "4.15.8"}]),
+]
+
+PARAMETRIZE_NEGATIVE_TESTS = [
+    ({"version": "4", "stream": "stable", "expected": "error"}),
+    ({"version": "100.5.1", "stream": "stable", "expected": "error"}),
+    ({"version": "100.5", "stream": "stable", "expected": "error"}),
+    ({"version": "4.15.40", "stream": "stable", "expected": "error"}),
+]
+
+
+@pytest.mark.parametrize(
+    "clusters",
+    PARAMETRIZE_TESTS,
+)
+def test_osd_cluster_version(clusters):
+    for cluster in clusters:
+        res = get_cluster_version_to_install(
+            wanted_version=cluster["version"],
+            base_versions_dict=ROSA_OSD_BASE_VERSIONS,
+            platform="aws-osd",
+            stream=cluster["stream"],
+            log_prefix="test-cluster-versions",
+            cluster_name="test-cluster",
+        )
+
+        assert res == cluster["expected"]
+
+
+@pytest.mark.parametrize(
+    "cluster",
+    PARAMETRIZE_NEGATIVE_TESTS,
+)
+def test_osd_cluster_version_negative(cluster):
+    with pytest.raises(click.Abort):
+        get_cluster_version_to_install(
+            wanted_version=cluster["version"],
+            base_versions_dict=ROSA_OSD_BASE_VERSIONS,
+            platform="aws-osd",
+            stream=cluster["stream"],
+            log_prefix="test-cluster-versions",
+            cluster_name="test-cluster",
+        )
+
+
+@pytest.mark.parametrize(
+    "clusters",
+    PARAMETRIZE_TESTS,
+)
+def test_rosa_cluster_version(clusters):
+    for cluster in clusters:
+        res = get_cluster_version_to_install(
+            wanted_version=cluster["version"],
+            base_versions_dict=ROSA_OSD_BASE_VERSIONS,
+            platform="rosa",
+            stream=cluster["stream"],
+            log_prefix="test-cluster-versions",
+            cluster_name="test-cluster",
+        )
+
+        assert res == cluster["expected"]
+
+
+@pytest.mark.parametrize(
+    "cluster",
+    PARAMETRIZE_NEGATIVE_TESTS,
+)
+def test_rosa_cluster_version_negative(cluster):
+    with pytest.raises(click.Abort):
+        get_cluster_version_to_install(
+            wanted_version=cluster["version"],
+            base_versions_dict=ROSA_OSD_BASE_VERSIONS,
+            platform="rosa",
+            stream=cluster["stream"],
+            log_prefix="test-cluster-versions",
+            cluster_name="test-cluster",
+        )
